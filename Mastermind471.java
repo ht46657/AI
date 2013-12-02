237daf1db1b537138fa1a7654c789dce498e3330
ht46657 <ht46657@umbc.edu>
2013-12-02 07:58:00 -0500

    started more shit

---
+++ Mastermind471.java
@@ -0,0 +1,9 @@
+package Project;
+
+public class Mastermind471
+{
+	
+	private int pegs = 0;
+	private int colors = 0;
+	
+}
--- Runner.java
+++ Runner.java
@@ -2,5 +2,49 @@ package Project;
 
 public class Runner
 {
-
+	public static void main(String[] args)
+	{
+		
+		System.out.println("1 - Mastermind");
+		System.out.println("2 - Traveling Salesman.");
+		System.out.println("3 - Exit.");
+		
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("Please select a game: ");
+			
+			try
+			{
+				int i = Integer.parseInt(s.next());
+				
+				if(i == 1)
+				{
+					System.out.println("You have selected Mastermind...");
+					Mastermind471 m = new Mastermind471();
+					s.close();
+					return;
+				}
+				else if(i == 2)
+				{
+					System.out.println("You have selected Traveling Salesman");
+					s.close();
+					return;
+				}
+				else if(i == 3)
+				{
+					System.out.println("Goodbye.");
+					s.close();
+					return;
+				}
+				
+				System.out.println();
+			}
+			catch(Exception e)
+			{
+				System.out.println("Please input a valid choice");
+			}
+		}
+	}
 }
